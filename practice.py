84059cbab2a4fd8991181242f35497d8b81bf29c
jozefdickins <30645291+jozefdickins@users.noreply.github.com>
2018-02-25 13:49:15 +0000

    not working yet, just modified the findSlice function

--- practice.py
+++ practice.py
@@ -13,14 +13,28 @@ C = param[1]
 L = param[2]
 H = param[3]
 
+slices = []
+
 
 def findSlice(pizza, xind, yind):
     numT = 0
     numM = 0
-    x = xind
-    y = yind
-    while(numT < L and numM < L and numT + numM < H):
-        
+    x = xind + 1
+    y = yind + 1
+    while(numT + numM < H):
+        while(numT < L or numM < L):
+            for n in range(xind, x):
+                for m in range(yind, y):
+                    if(pizza[n][m] == "T"):
+                        numT += 1
+                    elif(pizza[n][m] == "M"):
+                        numM += 1
+                    else:
+                        print("error counting Ts and Ms")
+            x += 1
+            y += 1
+    return (xind, yind, x, y)
+
 
 pizza = []
 
@@ -28,3 +42,12 @@ for x in range(0, len(lines)):
     lines[x] = lines[x].rstrip()
     if x > 0:
         pizza.append(list(lines[x]))
+
+
+firstSlice = findSlice(pizza, 0, 0)
+slices.append(firstSlice)
+
+
+print(len(slices))
+for pslice in slices:
+    print(pslice[0] + " " + pslice[1] + " " + pslice[2] + " " + pslice[3] + " ")
