a41e9bd4b7e424e4f22d0c00766f0761021894dd
jozefdickins <30645291+jozefdickins@users.noreply.github.com>
2018-02-25 12:37:33 +0000

    second

--- practice.py
+++ practice.py
@@ -8,10 +8,19 @@ with open(filename) as f_obj:
 
 param = lines[0].rstrip().split(' ')
 
-r = param[0]
-c = param[1]
-l = param[2]
-h = param[3]
+R = param[0]
+C = param[1]
+L = param[2]
+H = param[3]
+
+
+def findSlice(pizza, xind, yind):
+    numT = 0
+    numM = 0
+    x = xind
+    y = yind
+    while(numT < L and numM < L and numT + numM < H):
+        
 
 pizza = []
 
@@ -19,7 +28,3 @@ for x in range(0, len(lines)):
     lines[x] = lines[x].rstrip()
     if x > 0:
         pizza.append(list(lines[x]))
-
-for x in range(0, len(pizza)):
-    for y in range(0, len(pizza[x])):
-        print(pizza[x][y])
